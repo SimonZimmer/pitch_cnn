2f11cf78eaff77ac7961e90cb417b0715007615a
Simon Zimmermann <simon.zimmermann@rocketmail.com>
2018-10-23 09:02:31 +0200

    splitting in annotation and architecture

--- annotation.py
+++ annotation.py
@@ -11,21 +11,21 @@ import os
 # audio libraries
 from scipy.io import wavfile
 
-list_of_files = os.listdir("./test_training_set/")
-train_audio = np.zeros((16384, (list_of_files.__len__())), dtype=float)
-train_labels = np.zeros((1,(list_of_files.__len__())))
+list_of_files = os.listdir("./training_set/")
+train_audio = np.zeros((16384, list_of_files.__len__()))
+train_labels = np.zeros((1, list_of_files.__len__()))
 number_of_classes = 88
 
 # annotate train_labels / write train_audio
 i = 0
-for file in list_of_files:
-    path = "./test_training_set/" + file
-    data = wavfile.read(path)
-    train_audio[:, i] = data[1]
-    for rootnote in range(0, number_of_classes + 1):
-        if file.startswith("sinus" + str(rootnote) + "_"):
-            train_labels[:, i] = rootnote
-    i = i + 1
+#for file in list_of_files:
+ #   path = "./training_set/" + file
+  #  data = wavfile.read(path)
+#    train_audio[:, i] = data[1]
+   # for rootnote in range(0, number_of_classes + 1):
+    #    if file.startswith("sinus" + str(rootnote) + "_"):
+     #       train_labels[:, i] = rootnote
+   # i = i + 1
 
 np.save("/Users/simonzimmermann/dev/pitch_cnn/train_audio.npy", train_audio)
 np.save("/Users/simonzimmermann/dev/pitch_cnn/train_labels.npy", train_labels)
