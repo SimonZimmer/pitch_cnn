43a3d275a3e70b4b1f7d3903b239f6cca85f1db0
Simon Zimmermann <simon.zimmermann@rocketmail.com>
2018-11-08 15:39:48 +0100

    implementing fully convolutional architecture

--- annotation.py
+++ annotation.py
@@ -3,17 +3,16 @@ import tensorflow as tf
 
 # Helper libraries
 import numpy as np
-import scipy as sp
-from sklearn import preprocessing
 import os
-import random
+import pickle
+import keras.utils
 
 # audio libraries
 from scipy.io import wavfile
 
 list_of_files = os.listdir("./test_training_set/")
 num_files = list_of_files.__len__()
-number_of_classes = 88
+num_classes = 88
 training_data = np.zeros((num_files, 16384), dtype=float)
 labels = np.zeros((num_files, 1), dtype=int)
 
@@ -22,15 +21,25 @@ for i in range(0, num_files):
     path = "./test_training_set/" + list_of_files[i]
     temp_data = wavfile.read(path)
     training_data[i, :] = temp_data[1]
-    print(training_data[i,:])
-    for rootnote in range(0, number_of_classes + 1):
+    print(training_data[i, :])
+    for rootnote in range(0, num_classes + 1):
         if list_of_files[i].startswith("sinus" + str(rootnote) + "_"):
             labels[i, :] = rootnote
 
-import pickle
-pickle_out = open("X.pickle", "wb")
+# convert to one-hot matrix
+labels = keras.utils.to_categorical(labels, num_classes)
+
+# save np arrays using pickle
+pickle_out = open("x_train.pickle", "wb")
 pickle.dump(training_data, pickle_out)
 pickle_out.close()
-pickle_out = open("Y.pickle", "wb")
+pickle_out = open("y_train.pickle", "wb")
 pickle.dump(labels, pickle_out)
 pickle_out.close()
+
+# pickle_out = open("y_validate.pickle", "wb")
+# pickle.dump(labels[9 * int(labels.shape[0]/10):labels.shape[0]], pickle_out)
+# pickle_out.close()
+# pickle_out = open("x_validate.pickle", "wb")
+# pickle.dump(training_data[9 * int(training_data.shape[0]/10):training_data.shape[0]], pickle_out)
+# pickle_out.close()
--- architecture.py
+++ architecture.py
@@ -1,37 +1,146 @@
 
+# TensorFlow and tf.keras
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv1D, MaxPooling2D
+from tensorflow import keras
 
 # Helper libraries
 import numpy as np
 import pickle
+import matplotlib.pyplot as plt
 
 #load data
-pickle_in = open("X.pickle", "rb")
-x = pickle.load(pickle_in)
+pickle_in = open("x_train.pickle", "rb")
+x_train = pickle.load(pickle_in)
+pickle_in = open("y_train.pickle", "rb")
+y_train= pickle.load(pickle_in)
 
-pickle_in = open("Y.pickle", "rb")
-y = pickle.load(pickle_in)
+# normalize data
+x_train = np.abs(x_train / np.max(x_train))
+x_train = np.expand_dims(x_train, axis=2)
 
-# normalize
-x = np.abs(x / np.max(x))
-
-model = Sequential()
-model.add(Conv1D((32000, 1), 3))
-model.add(Activation("relu"))
-model.add(MaxPooling2D(pool_size=(4, 1)))
-
-model.add(Dense(1))
-model.add(Activation("sigmoid"))
-
-model.compile(loss="crossentropy",
-              optimizer="adam",
-              metrics=["accuracy"])
-
-model.fit(x, y)
+print("size of x_train", x_train.shape)
+print("size of y_train", y_train.shape)
 
 
+number = x_train.shape[0]
+size = x_train.shape[1]
+sample_rate = pow(2, 14)
+kernel_size = np.int(np.floor(sample_rate / 100))
+
+epochs = 10;
+batch_size = 128;
+num_classes = 88;
 
 
+# build model architecture
+model = keras.Sequential()
 
+model.add(keras.layers.Conv1D(input_shape=(size, 1),
+                              filters=48,
+                              kernel_size=kernel_size,
+                              strides=4))
+
+model.add(keras.layers.MaxPooling1D(pool_size=4))
+
+model.add(keras.layers.Conv1D(filters=48,
+                              kernel_size=3,
+                              strides=1,))
+
+model.add(keras.layers.Conv1D(filters=48,
+                              kernel_size=3,
+                              strides=1,))
+
+model.add(keras.layers.Conv1D(filters=48,
+                              kernel_size=3,
+                              strides=1,))
+
+model.add(keras.layers.MaxPooling1D(pool_size=4))
+
+model.add(keras.layers.Conv1D(filters=96,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=96,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=96,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=96,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.MaxPooling1D(pool_size=4))
+
+model.add(keras.layers.Conv1D(filters=192,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=192,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=192,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=192,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=192,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=192,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.MaxPooling1D(pool_size=4))
+
+model.add(keras.layers.Conv1D(filters=384,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=384,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.Conv1D(filters=384,
+                              kernel_size=3,
+                              strides=1))
+
+model.add(keras.layers.AveragePooling1D())
+
+model.add(keras.layers.Flatten())
+
+model.add(keras.layers.Dense(num_classes, activation='softmax'))
+
+
+model.compile(optimizer=tf.train.AdamOptimizer(),
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    validation_split=0.2,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1)
+
+plt.plot(history.history['acc'])
+plt.plot(history.history['val_acc'])
+plt.title('model accuracy')
+plt.xlabel('accuray')
+plt.ylabel('epochs')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.xlabel('loss')
+plt.ylabel('epochs')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
