c46901f6afb31b7a346d5494c7423ab6bfcc4438
Simon Zimmermann <simon.zimmermann@rocketmail.com>
2019-02-21 16:44:34 +0100

    adding generator class

---
+++ keras_script.py
@@ -0,0 +1,253 @@
+
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import numpy as np
+import keras
+import os
+import scipy.io.wavfile
+import re
+
+from sklearn.model_selection import train_test_split
+from custom_classes import DataGenerator
+
+start_note = 0
+end_note = 127
+num_classes = end_note - start_note + 1
+sample_rate = 16000
+init_kernel_size = np.int(np.floor(sample_rate / 100))
+epochs = 1000
+batch_size = 128
+
+
+def residual_block(y, nb_channels_in, nb_channels_out):
+
+    shortcut = y
+
+    y = keras.layers.Conv1D(nb_channels_in,
+                            kernel_size=3,
+                            strides=1,
+                            padding='same')(y)
+    y = batch_and_relu(y)
+
+    y = keras.layers.Conv1D(nb_channels_out,
+                            kernel_size=3,
+                            strides=1,
+                            padding='same')(y)
+
+    # batch normalization is employed after aggregating the transformations and before adding to the shortcut
+    y = keras.layers.BatchNormalization()(y)
+
+    y = keras.layers.add([shortcut, y])
+
+    # relu is performed right after each batch normalization,
+    # expect for the output of the block where relu is performed after the adding to the shortcut
+    y = keras.layers.ReLU()(y)
+
+    return y
+
+
+def batch_and_relu(x):
+    y = keras.layers.BatchNormalization()(x)
+    y = keras.layers.ReLU()(y)
+
+    return y
+
+
+def build_model(size):
+    print("building model architecture...")
+    size = (size, 1)
+    audio = keras.layers.Input(size)
+
+    # conv1
+    x = keras.layers.Conv1D(filters=48,
+                            kernel_size=init_kernel_size,
+                            strides=4)(audio)
+    x = batch_and_relu(x)
+    x = keras.layers.MaxPool1D(pool_size=4)(x)
+
+    # conv2
+    for i in range(3):
+        x = keras.layers.Conv1D(filters=48,
+                                kernel_size=3,
+                                strides=1,
+                                padding="same")(x)
+        x = batch_and_relu(x)
+    x = keras.layers.MaxPool1D(pool_size=4)(x)
+
+    # conv3
+    for i in range(4):
+        x = keras.layers.Conv1D(filters=96,
+                                kernel_size=3,
+                                strides=1,
+                                padding="same")(x)
+        x = batch_and_relu(x)
+    x = keras.layers.MaxPool1D(pool_size=4)(x)
+
+    # conv4
+    for i in range(6):
+        x = keras.layers.Conv1D(filters=192,
+                                kernel_size=3,
+                                strides=1,
+                                padding="same")(x)
+        x = batch_and_relu(x)
+    x = keras.layers.MaxPool1D(pool_size=4)(x)
+
+    # conv5
+    for i in range(3):
+        x = keras.layers.Conv1D(filters=384,
+                                kernel_size=3,
+                                strides=1,
+                                padding="same")(x)
+        x = batch_and_relu(x)
+
+    x = keras.layers.AveragePooling1D()(x)
+    x = keras.layers.Flatten()(x)
+    x = keras.layers.Dense(num_classes, activation='softmax')(x)
+
+    model = keras.Model(inputs=audio, outputs=x)
+
+    model.compile(optimizer=tf.train.AdamOptimizer(),
+                       loss='categorical_crossentropy',
+                       metrics=['accuracy'])
+
+    return model
+
+
+def train(model, x_train, y_train, epochs, with_plot):
+    early_stopping_monitor = keras.callbacks.EarlyStopping(patience=4)
+
+    history = model.fit(x_train, y_train,
+                        validation_split=0.2,
+                        batch_size=128,
+                        epochs=epochs,
+                        verbose=1,
+                        callbacks=[early_stopping_monitor])
+
+    if with_plot:
+        plt.plot(history.history['acc'])
+        plt.title('model accuracy')
+        plt.xlabel('epochs')
+        plt.ylabel('accuracy')
+        plt.show()
+
+        plt.plot(history.history['loss'])
+        plt.title('model loss')
+        plt.xlabel('epochs')
+        plt.ylabel('loss')
+        plt.show()
+
+    return history
+
+
+def load_data(num_classes):
+    print("load data...")
+    x_train = np.load("x_train.npy")
+    y_train = np.load("y_train.npy")
+    x_test = np.load("x_test.npy")
+    y_test = np.load("y_test.npy")
+    y_train = keras.utils.to_categorical(y_train, num_classes)
+    y_test = keras.utils.to_categorical(y_test, num_classes)
+    x_train = np.expand_dims(x_train, axis=2)
+    x_test = np.expand_dims(x_test, axis=2)
+
+    return x_train, y_train, x_test, y_test
+
+
+def annotate():
+    print("annotate data...")
+
+    seed = 7
+    np.random.seed(seed)
+
+    list_of_files = os.listdir("./main_training_set/")
+    num_files = list_of_files.__len__()
+
+    training_data = np.zeros((num_files, 4*sample_rate), dtype=float)
+    labels = np.zeros((num_files, 1), dtype=int)
+
+    for i in range(num_files):
+        if list_of_files[i] == '.DS_Store':
+            print('DS_Store item encountered & removed')
+            os.remove(list_of_files[i])
+
+        path = "./main_training_set/" + list_of_files[i]
+        print(path)
+        temp_data = scipy.io.wavfile.read(path)
+        print(len(temp_data[1]))
+        print(len(training_data[1,:]))
+        training_data[i, :] = temp_data[1]
+
+        # use regex to get pitch info from filename
+        pitch_regex = re.compile(r'(?<=-)(\d\d\d)(?=-)')
+        labels[i] = pitch_regex.findall(list_of_files[i])
+
+    x_train, x_test, y_train, y_test = train_test_split(training_data,
+                                                        labels,
+                                                        test_size=0.2,
+                                                        random_state=seed)
+    print("writing data to disk...")
+    np.save("x_train.npy", x_train)
+    np.save("y_train.npy", y_train)
+    np.save("x_test.npy", x_test)
+    np.save("y_test.npy", y_test)
+
+
+def save_model(name):
+    print("saving model...")
+    model_json = model.to_json()
+    with open("%s.json" % name, "w") as json_file:
+        json_file.write(model_json)
+
+    model.save_weights("%s.h5" % name)
+    print("Saved model to disk")
+
+
+def load_model(name):
+    print("loading model...")
+    json_file = open("%s.json" % name, 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+
+    loaded_model = tf.keras.models.model_from_json(
+        loaded_model_json
+    )
+
+    loaded_model.load_weights("%s.h5" % name)
+    print("Loaded model from disk")
+
+    return loaded_model
+
+
+def predict(model, x):
+    if isinstance(x, str):
+        x = scipy.io.wavfile.read(x)
+        x = x[1]
+        if x.shape[1] == 2:
+            x = x[:, :-1]
+        x = x[0:sample_rate, :]
+
+    x = np.expand_dims(x, axis=0)
+    y = model.predict(x)
+    indices = np.argmax(y, axis=1)
+    print("prediction = ")
+
+    return indices
+
+
+def evaluate(model):
+    model.compile(loss='binary_crossentropy',
+                        optimizer='rmsprop',
+                        metrics=['accuracy'])
+    cvscores = []
+    scores = model.evaluate(x_test, y_test, verbose=0)
+    print("%s: %.2f%%" % (model.metrics_names[1], scores[1] * 100))
+    cvscores.append(scores[1] * 100)
+
+
+# execution:
+annotate()
+x_train, y_train, x_test, y_test = load_data(num_classes)
+size = x_train.shape[1]
+model = build_model(size)
+model.summary()
+train(model, x_train, y_train, epochs, True)
